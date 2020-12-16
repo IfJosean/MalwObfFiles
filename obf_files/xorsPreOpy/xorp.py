afba8d6213b8c9f24fdb9549f7e49a6c1c509efe
IfJosean <32061994+IfJosean@users.noreply.github.com>
2020-12-15 20:30:27 -0500

    Add files via upload
    preopy xors

---
+++ obf_files/xorsPreOpy/xorp.py
@@ -0,0 +1,34 @@
+inputfile = open('malware','rb')
+outputfile = open('decode', 'w+b')
+byte = inputfile.read(1)
+counter = 0
+oper = 7
+operr = 7
+while byte != "":
+    byte = ord(byte)
+    oper = counter
+    if 1 ==1:
+      if counter % 5 ==1:
+          operr = counter % 17
+          operr += 2
+          operr = operr * 4
+          byte = byte ^ operr
+      if counter % 3 ==1:
+          operr = counter % 19
+          operr += 2
+          operr = operr * 5
+          byte = byte ^ operr
+      if counter % 2 ==1:
+          operr = counter % 47
+          operr += 2
+          operr = operr * 2
+          byte = byte ^ operr
+      else:
+          operr = counter % 31
+          operr += 2
+          operr = operr + 6
+          byte = byte ^ operr
+    outputfile.write('%c' % byte)
+    byte = inputfile.read(1)
+    counter += 1
+outputfile.close()
